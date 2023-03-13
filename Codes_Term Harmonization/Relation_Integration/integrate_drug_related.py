afcbec4974a17a2e66f28b043531ade7a71be599
houyurain <rain0613@hotmail.com>
2023-03-13 19:14:41 -0400

    Update integrate_drug_related.py

--- Codes_Term Harmonization/Relation_Integration/integrate_drug_related.py
+++ Codes_Term Harmonization/Relation_Integration/integrate_drug_related.py
@@ -1,21 +1,14 @@
-"""
-_*_ coding: utf-8 _*_
-@ Author: Yu Hou
-@File: integrate_drug_related.py
-@Time: 4/25/21 11:57 PM
-"""
-
 import pandas as pd
 
 
-folder = '/Users/yuhou/Documents/Knowledge_Graph/knowledge_bases_integration/'
+folder = ''
 
 
 def integrate_DPwy():
-    D_Pwy_kegg = pd.read_csv(folder + 'KEGG/kegg_drug_pathway.csv')
+    D_Pwy_kegg = pd.read_csv(folder + '/kegg_drug_pathway.csv')
     print(D_Pwy_kegg)
-    pathway_vocab = pd.read_csv(folder + 'v2_res_Apr2021_refine/res/entity/pathway_vocab.csv')
-    drug_vocab = pd.read_csv(folder + 'v2_res_Apr2021_refine/res/entity/drug_vocab.csv')
+    pathway_vocab = pd.read_csv(folder + '/pathway_vocab.csv')
+    drug_vocab = pd.read_csv(folder + '/drug_vocab.csv')
 
     pathway_primary_dict = pathway_vocab.dropna(subset=['kegg_id']).set_index('kegg_id')['primary'].to_dict()
     kegg_drug_vocab = drug_vocab.dropna(subset=['kegg_id'])
@@ -27,18 +20,18 @@ def integrate_DPwy():
     D_Pwy_res['Association'] = [1] * len(D_Pwy_res)
     D_Pwy_res['Source'] = ['KEGG'] * len(D_Pwy_res)
     print(D_Pwy_res)
-    D_Pwy_res.to_csv(folder + 'v2_res_Apr2021_refine/drug_related/D_Pw_res.csv', index=False)
+    D_Pwy_res.to_csv(folder + '/D_Pw_res.csv', index=False)
 
 
 def integrate_DSE():
-    sider_df = pd.read_table(folder + 'stage_4/entity/side_effect/meddra_all_se.tsv', header=None)
+    sider_df = pd.read_table(folder + '/meddra_all_se.tsv', header=None)
     sider_df = sider_df[sider_df[3] == 'PT']
     sider_df = sider_df[[0, 4, 5]]
     sider_df = sider_df.rename(columns={0: 'CID', 4: 'umls_cui', 5: 'name'})
     sider_df = sider_df.reset_index(drop=True)
 
-    drug_vocab = pd.read_csv(folder + 'v2_res_Apr2021_refine/res/entity/drug_vocab.csv')
-    side_effect_vocab = pd.read_csv(folder + 'v2_res_Apr2021_refine/res/entity/side_effect_vocab.csv')
+    drug_vocab = pd.read_csv(folder + '/drug_vocab.csv')
+    side_effect_vocab = pd.read_csv(folder + '/side_effect_vocab.csv')
 
     cid_primary = drug_vocab.dropna(subset=['CID']).set_index('CID')['primary'].to_dict()
     se_primary = side_effect_vocab.set_index(['umls_cui'])['primary'].to_dict()
@@ -51,17 +44,17 @@ def integrate_DSE():
     D_SE_res = D_SE_res.rename(columns={'CID': 'Drug', 'umls_cui': 'Side_Effect'})
 
     print(D_SE_res)
-    D_SE_res.to_csv(folder + 'v2_res_Apr2021_refine/drug_related/D_SE_res.csv', index=False)
+    D_SE_res.to_csv(folder + '/D_SE_res.csv', index=False)
 
 
 def integrate_DSDSI():
-    sdsi_spd = pd.read_table(folder + 'stage_3/idisk-rrf-1.0.1-2020-01-28/MRREL.RRF', delimiter='|')
+    sdsi_spd = pd.read_table(folder + '/MRREL.RRF', delimiter='|')
     sdsi_spd = sdsi_spd[sdsi_spd['REL'] == 'interacts_with']
     sdsi_spd = sdsi_spd.reset_index(drop=True)
 
-    sdsi_vocab = pd.read_csv(folder + 'v2_res_Apr2021_refine/res/entity/sdsi_vocab.csv')
+    sdsi_vocab = pd.read_csv(folder + '/sdsi_vocab.csv')
     sdsi_primary_dict = sdsi_vocab.set_index('iDISK_id')['primary'].to_dict()
-    drug_vocab = pd.read_csv(folder + 'v2_res_Apr2021_refine/res/entity/drug_vocab.csv')
+    drug_vocab = pd.read_csv(folder + '/drug_vocab.csv')
     drug_idisk_primary_dict = drug_vocab.dropna(subset=['iDISK_id']).set_index('iDISK_id')['primary'].to_dict()
 
     sdsi_spd_res = pd.DataFrame(columns=['SDSI', 'Drug', 'interacts_with', 'Source'])
@@ -73,7 +66,7 @@ def integrate_DSDSI():
         sdsi_spd_res.loc[i] = [sdsi_primary, drug_primary, 1, 'iDISK']
         print(i + 1, '/', len(sdsi_spd), 'Completed (SDSI_SPD)...')
 
-    sdsi_spd_res.to_csv(folder + 'v2_res_Apr2021_refine/drug_related/SDSI_D_res.csv', index=False)
+    sdsi_spd_res.to_csv(folder + '/SDSI_D_res.csv', index=False)
 
 
 def main():
